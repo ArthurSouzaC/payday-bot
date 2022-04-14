fef12122453a549600f171221605ffd0c0fbd462
ArthurSouzaC <arthursouza.info@gmail.com>
2022-04-14 13:36:45 -0300

    peidei easter egg

--- commands/index.js
+++ commands/index.js
@@ -1,5 +1,7 @@
 import payday from './payday.js';
+import peidei from './peidei.js'
 
 export default {
-    payday
+    payday,
+    peidei
 }
---
+++ commands/peidei.js
@@ -0,0 +1,5 @@
+export default {
+    name: '!peidei',
+    description: '?',
+    execute: (msg) => { msg.channel.reply("Corno!") }
+}
