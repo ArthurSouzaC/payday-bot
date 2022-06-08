381287b3c5e74b96a128d5ae1eabbf1dd4d2e54a
Arthur Alves de Souza Costa <arthursouza.info@gmail.com>
2022-06-08 09:04:44 -0300

    Update paydayService.js

--- services/paydayService.js
+++ services/paydayService.js
@@ -1,7 +1,7 @@
 import { payDay } from '../holidays/script.js';
 
 export const getPayday = async (msg) => {
-    let result = await payDay()
+    let result = await payDay(true)
     msg.channel.send(`🔥🎆 Faltam ${result.remainingDays} dias até o pagamento! 🎆🔥`);
     msg.channel.send(`Data do próximo pagamento: ${result.nextPayDay}`);
 }
